74b0f899d0e2299f532d457d4091519157186665
Eduardo <57434811+NoobToDaMaximum@users.noreply.github.com>
2020-12-11 20:04:34 +0100

    Leer accesoMemoria.txt
    Leo el fichero y lo asigno a numero

--- Proyecto1/CACHEsym.c
+++ Proyecto1/CACHEsym.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-  unsigned char RAM[1024];
-  FILE *f = fopen("RAM.bin","r");
-  FILE *f2 = fopen("accesos_memoria.txt", "r"):
+typedef struct{
+	short int ETQ;
+	short int Datos[8];
+}T_LINEA_CACHE;
+
+int variableglobal = 0;
+
+int main (){
+	int numfallos = 0;
+	unsigned char RAM[1024];
+	char leerLinea[4];
+	int numero = 0;
+	
+	T_LINEA_CACHE test;
+	test.ETQ == 0xFF;
+	
+	FILE *f = fopen("RAM.bin","r");
+	
+	FILE *f2 = fopen("accesos_memoria.txt", "r");
+	while(fscanf(f2, "%s",leerLinea) != EOF){
+		
+		printf("LINEA: %s\n",leerLinea);
+		sscanf(leerLinea, "%x", &numero);
+		printf("LINEA (int): %x\n", numero);
+		
+		if(numero != test.ETQ){
+			numfallos++;
+			variableglobal += 10;
+			printf("T: %d, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X");
+		}
+	}fclose(f2);
+  
+	return 0;
 }
