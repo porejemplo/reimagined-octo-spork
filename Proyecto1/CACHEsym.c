ae1c9c4092d69d43e8804b2ea66b571f02261414
Eduardo <57434811+NoobToDaMaximum@users.noreply.github.com>
2020-12-11 20:27:52 +0100

    Control de error
    Control de error al leer el fichero. Se sale de la aplicacion al no encontrar el fichero.

--- Proyecto1/CACHEsym.c
+++ Proyecto1/CACHEsym.c
@@ -10,17 +10,29 @@ int variableglobal = 0;
 
 int main (){
 	int numfallos = 0;
-	unsigned char RAM[1024];
-	char leerLinea[4];
+	unsigned char RAM[1024] = {'\0'};
+	char leerLinea[4]= {'\0'};
 	int numero = 0;
 	
 	T_LINEA_CACHE test;
-	test.ETQ == 0xFF;
+	test.ETQ = 0xFF;
+	for(int i = 0; i < 8; i++){
+		test.Datos[i] = 0;
+	}
 	
-	FILE *f = fopen("RAM.bin","r");
+	FILE *ficheroRam = fopen("RAM.bin","r");
+	if(ficheroRam == NULL){
+		exit(-1);
+	}
+	while(fscanf(ficheroRam,"%s",RAM)!=EOF){
+		printf("LINEA RAM: %s\n", RAM);
+	}fclose(ficheroRam);
 	
-	FILE *f2 = fopen("accesos_memoria.txt", "r");
-	while(fscanf(f2, "%s",leerLinea) != EOF){
+	FILE *ficheroMemoria = fopen("accesos_memoria.txt", "r");
+	if(ficheroMemoria == NULL){
+		exit(-1);
+	}
+	while(fscanf(ficheroMemoria, "%s",leerLinea) != EOF){
 		
 		printf("LINEA: %s\n",leerLinea);
 		sscanf(leerLinea, "%x", &numero);
@@ -31,7 +43,7 @@ int main (){
 			variableglobal += 10;
 			printf("Nº Fallos: %d\nNº Variableglobal: %d\n", numfallos, variableglobal);
 		}
-	}fclose(f2);
+	}fclose(ficheroMemoria);
   
 	return 0;
 }
