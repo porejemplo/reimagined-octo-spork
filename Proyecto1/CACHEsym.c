e275844636994dc121e73602c5c2b5997e9e2f20
Eduardo <57434811+NoobToDaMaximum@users.noreply.github.com>
2020-12-12 16:26:35 +0100

    Constantes

--- Proyecto1/CACHEsym.c
+++ Proyecto1/CACHEsym.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#define SLEEP 2
 
 typedef struct{
 	short int ETQ;
@@ -101,7 +102,7 @@ int main (){
 				break;
 			}
 		}
-		sleep(2);
+		sleep(SLEEP);
 	}
 	fclose(ficheroMemoria);
 	
